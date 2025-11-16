e1294e316e9595ee1404d5a2550841b1a2af9b2d
kero <kerosamy42004@gmail.com>
2025-11-16 22:14:25 +0200

    project initialization

---
+++ emergency-dispatch-system-backend/src/main/java/com/example/emergency_dispatch_system_backend/EmergencyDispatchSystemBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.emergency_dispatch_system_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmergencyDispatchSystemBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmergencyDispatchSystemBackendApplication.class, args);
+	}
+
+}
---
+++ emergency-dispatch-system-backend/src/test/java/com/example/emergency_dispatch_system_backend/EmergencyDispatchSystemBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.emergency_dispatch_system_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmergencyDispatchSystemBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
