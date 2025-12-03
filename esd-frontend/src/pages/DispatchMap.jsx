4043485893679b08df46fac400be7d9970a37d53
John William <johnwilliamfawzy@gmail.com>
2025-12-03 06:12:31 +0200

    edit map styling

--- esd-frontend/src/pages/DispatchMap.jsx
+++ esd-frontend/src/pages/DispatchMap.jsx
@@ -81,7 +81,8 @@ const DispatchMap = () => {
           */}
           <TileLayer
             attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>'
-url="https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png"          />
+            url="https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png"
+          />
 
           {/* Render Stations */}
           {stations.map(station => (
